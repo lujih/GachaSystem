7b62cda9f72a30d7e3e4833e8fb45b648c805170
cszx <3369661439@qq.com>
2026-02-11 23:34:35 +0800

    已修复图片加载

--- worker.js
+++ worker.js
@@ -3382,7 +3382,7 @@ function getHtmlPage() {
                this.loading = false;
               
               const icon = this.currentPool === 'ltd' ? 'fa-star' : 'fa-bolt';
-              if (btn) btn.innerHTML = `<i class="fas ${icon}"></i> 再召唤`;
+              if (btn) btn.innerHTML = \`<i class="fas \${icon}"></i> 再召唤\`;
 
               if (data.rarity && tag) { 
                   tag.innerText = data.rarity; 
