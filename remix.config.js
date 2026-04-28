e1a59bffd93f67c3290a8ad2fc2d9d5c6d1ea939
cszx <3369661439@qq.com>
2026-04-28 23:07:42 +0800

    fix: remove unnecessary server field from remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   serverBuildTarget: "cloudflare-pages",
-  server: "./server.js",
   serverBuildPath: "functions/[[path]].js",
   ignoredRouteFiles: ["**/*.css"],
 };
