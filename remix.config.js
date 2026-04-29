296ed554ff5fb08fc66a878a5494a37a0850f387
cszx <3369661439@qq.com>
2026-04-29 22:25:32 +0800

    fix: move serverBuildPath to vite.config.ts, remove remix.config.js

--- remix.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  serverBuildTarget: "cloudflare-pages",
-  serverBuildPath: "functions/[[path]].js",
-  ignoredRouteFiles: ["**/*.css"],
-};
